2642d6db89a84e8e4071e272ccd573ab58ae297d
mlkelly <maurak87@gmail.com>
2020-06-19 08:30:04 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,15 @@
 class ArtistsController < ApplicationController
+    set :views, Proc.new { File.join(root, "../views/artists") }
+
+    get '/artists' do
+        @artists = Artist.all
+        erb :index
+    end
+
+    get '/artists/:slug' do 
+        @artist = Artist.find_by_slug(params[:slug])
+        @genres = @artist.genres
+        @songs = @artist.songs
+        erb :show
+    end
 end
